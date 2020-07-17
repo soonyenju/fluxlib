72ac93ddb1ae5d194edc4748f0c3f6c0397ed7f6
Songyan Zhu <zhusy93@gmail.com>
2020-07-17 09:10:05 +0100

    Update __init__.py

--- fluxlib/__init__.py
+++ fluxlib/__init__.py
@@ -4,11 +4,9 @@ from . import preprocessing
 from .preprocessing import *
 from . import partitioning
 from .partitioning import *
-from . import functions
-from .functions import *
+from . import funcs
 
-__all__ = ["fluxnet", "gapfill"]
+__all__ = ["fluxnet", "gapfill", "funcs"]
 __all__ += gapfill.__all__
 __all__ += preprocessing.__all__
 __all__ += partitioning.__all__
-__all__ += functions.__all__
