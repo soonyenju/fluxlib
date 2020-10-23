7dd773335101850fb331887a644ed5c134a33afb
Zhu <sz394@exeter.ac.uk>
2020-10-23 08:55:44 +0100

    update

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name = "fluxlib",
-	version = "0.0.12",
+	version = "0.0.13",
 	keywords = ("eddy covariance postprocessing, gapfilling and partitioning", "flux"),
 	description = "left blank",
 	long_description = "left blank",
