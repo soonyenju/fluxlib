bfc66f0a8c72f1cb9d85fecfe27ddf4523213044
Songyan Zhu <zhusy93@gmail.com>
2022-09-07 09:18:37 +0100

    Update __init__.py

--- fluxlib/gapfill/__init__.py
+++ fluxlib/gapfill/__init__.py
@@ -1,5 +1,6 @@
 from .dataloader import Loader
 from .ggapfill import GFiller
 from .auxfill_xgb import AuxFiller
+from .gapfill_routine import create_gapfill, artificial_gaps, gapfill_pipeline, GapfillReport
 
-__all__ = ["Loader", "GFiller", "utils", "AuxFiller"]
+__all__ = ["Loader", "GFiller", "utils", "AuxFiller", "create_gapfill", "artificial_gaps", "gapfill_pipeline", "GapfillReport"]
