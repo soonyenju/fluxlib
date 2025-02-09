d706641957bd19d614cc382542d860c5c81f25fb
Songyan Zhu <zhusy93@gmail.com>
2025-02-09 09:55:57 +0000

    Update __init__.py

--- fluxlib/__init__.py
+++ fluxlib/__init__.py
@@ -1,5 +1,5 @@
-from . import gapfill
-from .gapfill import *
+from . import gapfilling
+from .gapfilling import *
 from . import preprocessing
 from .preprocessing import *
 from . import partitioning
@@ -7,9 +7,9 @@ from .partitioning import *
 from . import toolbox
 from .toolbox import *
 from .ustar_filtering import ustarfilter
+from . import preprocessing2
 
-__all__ = ["gapfill", "toolbox", "partitioning", "ustarfilter"]
-__all__ += gapfill.__all__
+__all__ = ["gapfilling", "toolbox", "partitioning", "ustarfilter", "preprocessing2"]
 __all__ += preprocessing.__all__
 __all__ += partitioning.__all__
 __all__ += toolbox.__all__
