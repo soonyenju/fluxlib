2a25e1c640ac42e0d36d85cd62087247593ac0c4
Songyan Zhu <zhusy93@gmail.com>
2025-04-18 20:44:55 +0100

    Update __init__.py

--- fluxlib/__init__.py
+++ fluxlib/__init__.py
@@ -5,6 +5,7 @@ from .partitioning import *
 from .ustar_filtering import ustarfilter
 from . import partitioning2
 from . import eddyflux
+from . import bigleaf
 
-__all__ = ["gapfilling", "partitioning", "ustarfilter", "partitioning2", "eddyflux"]
+__all__ = ["gapfilling", "partitioning", "ustarfilter", "partitioning2", "eddyflux", "bigleaf"]
 __all__ += partitioning.__all__
