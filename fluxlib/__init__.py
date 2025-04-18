fad970cf628514cba792fe74d38ec87584776225
Songyan Zhu <zhusy93@gmail.com>
2025-04-18 20:05:39 +0100

    Update __init__.py

--- fluxlib/__init__.py
+++ fluxlib/__init__.py
@@ -4,6 +4,7 @@ from . import partitioning
 from .partitioning import *
 from .ustar_filtering import ustarfilter
 from . import partitioning2
+from . import eddyflux
 
-__all__ = ["gapfilling", "partitioning", "ustarfilter", "partitioning2"]
+__all__ = ["gapfilling", "partitioning", "ustarfilter", "partitioning2", "eddyflux"]
 __all__ += partitioning.__all__
